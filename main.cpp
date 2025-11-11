5cd0e537eaae5e0f4d5b7a9e5f2e8ced4d113b01
tddgit096-pc <>
2025-11-10 17:32:19 -0800

    initial commit w/ instructions for assignment.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Car.h"
+#include <deque>
+
+using namespace std;
+/*
+[Milestone 1] Your code successfully creates a std::deque and populates it with two Car objects. Your code should exercise appropriate deque methods on those objects.
+[Milestone 2] Your code successfully incorporates a loop that runs until the deque is empty. Inside the loop, you branch based on the probabilities.
+*/
+
+
+main(){
+
+    //calling default car constructor returns randomized car.
+
+    //55% probability that the car at the head of the line pays its toll and leaves the toll booth
+    //45% probability that another car joins the line for the toll booth
+
+    //run until deque is empty
+    //after each simulation, display queue via car's print method
+
+}
