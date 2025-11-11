0175c9114c7664fa70c383ad89813a3fd154eafc
tddgit096-pc <>
2025-11-10 17:50:49 -0800

    progress on logic

--- main.cpp
+++ main.cpp
@@ -6,25 +6,48 @@ using namespace std;
 
 const int INITIALCARS = 2;
 /*
-[Milestone 1] Your code successfully creates a std::deque and populates it with two Car objects. Your code should exercise appropriate deque methods on those objects.
 [Milestone 2] Your code successfully incorporates a loop that runs until the deque is empty. Inside the loop, you branch based on the probabilities.
 */
-
+void print_deque(deque<Car>);
 
 int main(){
+    srand(time(NULL));
     deque<Car> Toll;
+    int time = 1;
     for(int i=0;i< INITIALCARS;i++){
         Toll.push_back(Car());    //calling default car constructor returns randomized car.
     }
+    cout<<"Initial queue:\n";
 
-    for(Car c : Toll){//DEBUG CODE
-        c.print();
+    while(!Toll.empty()){
+        //printing:
+        cout<<"Time: "<< time <<" Operation: ";
+        if(rand()%55<100){    //55% probability that the car at the head of the line pays its toll and leaves the toll booth
+            cout<<"Car Paid: ";
+               
+        }    
+    
     }
 
-    //55% probability that the car at the head of the line pays its toll and leaves the toll booth
+
+
+
+
     //45% probability that another car joins the line for the toll booth
 
     //run until deque is empty
     //after each simulation, display queue via car's print method
     return 0;
+}
+
+void print_deque(deque<Car> D){
+    cout<<"Queue:\n";
+    if(D.empty()){
+        cout<<"\tEmpty.\n";
+        return;
+    }
+    for(Car c: D){
+        cout<<"\t";
+        c.print();
+    }
 }
