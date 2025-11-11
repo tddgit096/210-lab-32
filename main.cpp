c7d2a2e0e42a851a1063f6fdcf2d734d0d125248
tddgit096-pc <>
2025-11-10 18:32:50 -0800

    initial commit

--- main.cpp
+++ main.cpp
@@ -3,8 +3,38 @@
 #include <deque>
 
 using namespace std;
+/*
+    Add multiple lanes into the management system to simulate an entire plaza of toll booths
+    Add the capability for a car at the rear of a lane to switch lanes
 
-const int INITIALCARS = 2;
+    There are now 4 toll booth lanes at play. Use an array to hold the lanes. The array will be of type deque<Car>. When the simulation starts, pre-populate the lanes with 2 cars like before. Each time period will now have operations happening on all lines.
+    Any car at the end of any queue can switch lanes to a random lane that's not their original lane.
+
+    The three possible operations and their probabilities are:
+    46% probability that the car at the head of the queue pays its toll and leaves the queue
+    39% probability that another car joins the queue
+    15% probability that the rear car will shift lanes
+
+    All these probability values need to be stored as constants defined in your program, never in hard-coding.
+    If a lane is currently empty but there is still more time in the simulation, those probabilities will be just 50/50 if a new car enters the queue or not. Of course another car might shift lanes into it, but that's a different lane's business.
+
+    Run the simulation for 20 time periods.
+
+    In addition to your 10-minute commits, do an additional commit when reaching these milestones:
+
+    [Milestone 1] You have branched from Lab 32.
+
+    [Milestone 2] Your code has created the array of deques, and tests operations on this complex data structure to validate it.
+
+    [Milestone 3] Your code populates the plaza with 2 cars before the simulation runs. Output the current deque status per the sample output.
+
+    [Milestone 4] Your code features the loop. Inside the loop, only code two probabilities: 50/50 of the car paying/leaving and of the car joining the deque. Your code should fully exercise the data structures.
+
+    [Milestone 5] Your code supports lane switching and fully exercises the data structures.
+
+    */
+const int INITIALCARS = 2, SIMULATIONTIME = 20;
+const int PAYPROBABILITY = 46, JOINPROBABILITY = 39;
 
 void print_deque(deque<Car>);
 
