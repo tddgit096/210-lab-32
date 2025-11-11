33bdccb1cb382408f67f8bba1b765adb330a9c0c
tddgit096-pc <>
2025-11-10 17:58:41 -0800

    Milestone 2

--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@
 using namespace std;
 
 const int INITIALCARS = 2;
-/*
-[Milestone 2] Your code successfully incorporates a loop that runs until the deque is empty. Inside the loop, you branch based on the probabilities.
-*/
+
 void print_deque(deque<Car>);
 
 int main(){
@@ -15,28 +13,25 @@ int main(){
     deque<Car> Toll;
     int time = 1;
     for(int i=0;i< INITIALCARS;i++){
-        Toll.push_back(Car());    //calling default car constructor returns randomized car.
+        Toll.push_back(Car());          //calling default car constructor returns randomized car.
     }
     cout<<"Initial queue:\n";
-
-    while(!Toll.empty()){
-        //printing:
+    print_deque(Toll);     
+    while(!Toll.empty()){               //run until deque is empty
         cout<<"Time: "<< time <<" Operation: ";
-        if(rand()%55<100){    //55% probability that the car at the head of the line pays its toll and leaves the toll booth
+        if(rand()%100<55){              //55% probability that the car at the head of the line pays its toll and leaves the toll booth
             cout<<"Car Paid: ";
-               
-        }    
-    
+            Toll.front().print();
+            Toll.pop_front();
+        }
+        else{
+            cout<<"Joined lane: ";      //45% probability that another car joins the line for the toll booth
+            Toll.back().print();
+            Toll.push_back(Car());
+        }
+        print_deque(Toll);              //after each simulation, display queue via car's print method
+        time++;
     }
-
-
-
-
-
-    //45% probability that another car joins the line for the toll booth
-
-    //run until deque is empty
-    //after each simulation, display queue via car's print method
     return 0;
 }
 
